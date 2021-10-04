5b8c0d96992c7d20cdc90c3f3268632384462fd1
mostaphaRoudsari <sadeghipour@gmail.com>
2021-10-03 23:56:03 -0400

    feat(grid): add functions for better grid distribution

--- tests/grid_test.py
+++ tests/grid_test.py
@@ -1,4 +1,5 @@
-from pollination.honeybee_radiance.grid import SplitGrid, MergeFiles
+from pollination.honeybee_radiance.grid import SplitGrid, MergeFiles, \
+    SplitGridFolder, MergeFolderData
 from queenbee.plugin.function import Function
 
 
@@ -11,4 +12,16 @@ def test_split_grid():
 def test_merge_files():
     function = MergeFiles().queenbee
     assert function.name == 'merge-files'
+    assert isinstance(function, Function)
+
+
+def test_split_folder():
+    function = SplitGridFolder().queenbee
+    assert function.name == 'split-grid-folder'
+    assert isinstance(function, Function)
+
+
+def test_merge_folder():
+    function = MergeFolderData().queenbee
+    assert function.name == 'merge-folder-data'
     assert isinstance(function, Function)
