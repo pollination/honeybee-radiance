cef7ffd1aeca9200fd47c6a8a621d2218a44c8c1
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-03-10 11:04:38 +0100

    refactor(glare): Merge functions and optional schedule

--- pollination/honeybee_radiance/glare.py
+++ pollination/honeybee_radiance/glare.py
@@ -3,7 +3,7 @@ from pollination_dsl.function import Function, command, Inputs, Outputs
 
 
 @dataclass
-class DCGlareGlareAutonomy(Function):
+class DCGlareDGA(Function):
     """Calculates glare autonomy. The glare autonomy is the fraction of hours (occupied) 
     without any detected glare. The detection of glare is controlled by glare_limit."""
 
@@ -31,8 +31,9 @@ class DCGlareGlareAutonomy(Function):
         description='Constant threshold factor in cd/m2.', default=2000
     )
 
-    occupancy_scheulde = Inputs.file(
+    occupancy_schedule = Inputs.file(
         description='Path to occupancy schedule.', path='occupancy_schedule.csv',
+        optional=True
     )
 
     @command
@@ -40,16 +41,18 @@ class DCGlareGlareAutonomy(Function):
         return 'honeybee-radiance dcglare two-phase dc_direct.mtx dc_total.mtx ' \
             'sky.smx view_rays.ray --glare-limit {{self.glare_limit}} ' \
             '--threshold-factor "{{self.threshold_factor}}" --occupancy-schedule ' \
-            '{{self.occupancy_schedule}} --output occupied.ga'
+            '{{self.occupancy_schedule}} --output view_rays.ga'
 
-    dcglare_glare_autonomy = Outputs.file(
-        description='Output dcglare glare autonomy file.', path='occupied.ga'
+    view_rays_glare_autonomy = Outputs.file(
+        description='Output dcglare glare autonomy file.', path='view_rays.ga'
     )
 
 
 @dataclass
-class DCGlare(Function):
-    """Calculates DGP for all sky conditions in the sky matrix."""
+class DCGlareDGP(Function):
+    """Calculates DGP for all sky conditions in the sky matrix, but filtered by an
+    occupancy schedule. This means that unoccupied hours will be zero DGP. If the
+    occupancy scheudle does not """
 
     dc_direct = Inputs.file(
         description='Path to dcdirect.', path='dc_direct.mtx',
@@ -71,52 +74,17 @@ class DCGlare(Function):
         description='Constant threshold factor in cd/m2.', default=2000
     )
 
+    occupancy_schedule = Inputs.file(
+        description='Path to occupancy schedule.', path='occupancy_schedule.csv',
+        optional=True
+    )
+
     @command
     def run_dcglare(self):
         return 'honeybee-radiance dcglare two-phase dc_direct.mtx dc_total.mtx ' \
             'sky.smx view_rays.ray --threshold-factor "{{self.threshold_factor}}" ' \
-            '--output view_rays.dgp'
+            '--occupancy-schedule {{self.occupancy_schedule}} --output view_rays.dgp'
 
     view_rays_dgp = Outputs.file(
         description='Output dcglare dgp file.', path='view_rays.dgp'
-    )
-
-
-@dataclass
-class DCGlareOccupancy(Function):
-    """Calculates DGP for all sky conditions in the sky matrix, but filtered by an
-    occupancy schedule. This means that unoccupied hours will be zero DGP."""
-
-    dc_direct = Inputs.file(
-        description='Path to dcdirect.', path='dc_direct.mtx',
-    )
-
-    dc_total = Inputs.file(
-        description='Path to dctotal.', path='dc_total.mtx',
-    )
-
-    sky_vector = Inputs.file(
-        description='Path to sky vector.', path='sky.smx'
-    )
-
-    view_rays = Inputs.file(
-        description='Path to view ray.', path='view_rays.ray',
-    )
-
-    threshold_factor = Inputs.float(
-        description='Constant threshold factor in cd/m2.', default=2000
-    )
-
-    occupancy_scheulde = Inputs.file(
-        description='Path to occupancy schedule.', path='occupancy_schedule.csv',
-    )
-
-    @command
-    def run_dcglare(self):
-        return 'honeybee-radiance dcglare two-phase dc_direct.mtx dc_total.mtx ' \
-            'sky.smx view_rays.ray --threshold-factor "{{self.threshold_factor}}" ' \
-            '--occupancy-schedule {{self.occupancy_schedule}} --output occupied.dgp'
-
-    occupied_dgp = Outputs.file(
-        description='Output dcglare occupied dgp file.', path='occupied.dgp'
     )
