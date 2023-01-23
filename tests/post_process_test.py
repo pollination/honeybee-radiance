2e6b61bcda37663d0d7200cc4d11d7b350be8585
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-01-23 13:48:08 +0100

    fix(postprocess): Add vis-metadata tests

--- tests/post_process_test.py
+++ tests/post_process_test.py
@@ -1,6 +1,10 @@
+import os
+
+from ladybug.futil import nukedir
 from pollination.honeybee_radiance.post_process import ConvertToBinary, SumRow, \
     AnnualIrradianceMetrics, AnnualDaylightMetrics, LeedIlluminanceCredits, \
-    SolarTrackingSynthesis, DaylightFactorConfig
+    SolarTrackingSynthesis, DaylightFactorConfig, DaylightFactorVisMetadata, \
+    ImagelessAnnualGlareVisMetadata
 
 from queenbee.plugin.function import Function
 
@@ -45,3 +49,33 @@ def test_daylight_factor_config():
     function = DaylightFactorConfig().queenbee
     assert function.name == 'daylight-factor-config'
     assert isinstance(function, Function)
+
+
+def test_daylight_factor_vis_metadata():
+    function = DaylightFactorVisMetadata()
+    qb_function = function.queenbee
+    assert qb_function.name == 'daylight-factor-vis-metadata'
+    assert isinstance(qb_function, Function)
+
+    inputs = {} # inputs is empty for this function
+    folder = './tests/assets/temp'
+    output_file = os.path.join(folder, 'vis_metadata.json')
+    function._try(inputs, folder=folder)
+    assert os.path.isfile(output_file)
+
+    nukedir(folder)
+
+
+def test_imageless_annual_glare_vis_metadata():
+    function = ImagelessAnnualGlareVisMetadata()
+    qb_function = function.queenbee
+    assert qb_function.name == 'imageless-annual-glare-vis-metadata'
+    assert isinstance(qb_function, Function)
+
+    inputs = {} # inputs is empty for this function
+    folder = './tests/assets/temp'
+    output_file = os.path.join(folder, 'vis_metadata.json')
+    function._try(inputs, folder=folder)
+    assert os.path.isfile(output_file)
+
+    nukedir(folder)
