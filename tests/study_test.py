9e4aa23d8d1ee01ddf36080c6f8a46650bda94bc
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-08-20 18:29:20 +0200

    test(study): Add test for epw and wea

--- tests/study_test.py
+++ tests/study_test.py
@@ -12,6 +12,37 @@ def test_study_info_function():
     assert isinstance(function, Function)
 
 
+
+def test_study_info_wea():
+    function = StudyInfo()
+    inputs = {
+        'wea': './tests/assets/study-info/wea.wea',
+        'timestep': 1
+    }
+    folder = Path('./tests/assets/temp')
+    if not folder.exists():
+        folder.mkdir(parents=True)
+    output = folder.joinpath('study_info.json')
+    function._try(inputs=inputs, folder=folder)
+    assert output.exists()
+    nukedir(folder)
+
+
+def test_study_info_epw():
+    function = StudyInfo()
+    inputs = {
+        'wea': './tests/assets/study-info/sky.epw',
+        'timestep': 1
+    }
+    folder = Path('./tests/assets/temp')
+    if not folder.exists():
+        folder.mkdir(parents=True)
+    output = folder.joinpath('study_info.json')
+    function._try(inputs=inputs, folder=folder)
+    assert output.exists()
+    nukedir(folder)
+
+
 def test_study_info_timestep_2():
     function = StudyInfo()
     inputs = {
